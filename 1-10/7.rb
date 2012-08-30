95d29d09635b33b478dd6ebe43e580ac81a0ffbc
Takanobu Nagumo <bunokatamoguna@gmail.com>
2012-08-30 14:10:38 +0900

    Update 1-10/7.rb
    fixed indents.

--- 1-10/7.rb
+++ 1-10/7.rb
@@ -2,20 +2,20 @@ primes = Enumerator.new do |y|
   number = 2
   loop do
     y << number
-	if number == 2
-	  number = 3
-	else
-	  loop do
-	    number += 2
-		result = catch(:done) do
-		  for i in 2..Integer(Math.sqrt(number))
-		    throw(:done, false) if (number % i).zero?
-		  end
-		  throw(:done, true)
-		end
-		break if result
+    if number == 2
+      number = 3
+    else
+      loop do
+        number += 2
+        result = catch(:done) do
+          for i in 2..Integer(Math.sqrt(number))
+            throw(:done, false) if (number % i).zero?
+          end
+          throw(:done, true)
+        end
+        break if result
       end
-	end
+    end
   end
 end
 
