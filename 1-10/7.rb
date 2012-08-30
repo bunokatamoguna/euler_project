ddec0c9663b8667b2e1354fbdbb1e87ece29be9a
bunokatamoguna <bunokatamoguna@gmail.com>
2012-08-30 14:06:45 +0900

    solved problem 7

---
+++ 1-10/7.rb
@@ -0,0 +1,22 @@
+primes = Enumerator.new do |y|
+  number = 2
+  loop do
+    y << number
+	if number == 2
+	  number = 3
+	else
+	  loop do
+	    number += 2
+		result = catch(:done) do
+		  for i in 2..Integer(Math.sqrt(number))
+		    throw(:done, false) if (number % i).zero?
+		  end
+		  throw(:done, true)
+		end
+		break if result
+      end
+	end
+  end
+end
+
+p primes.first(10001).last
